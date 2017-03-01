a36890581874647aa65c120e8ff2cea43f9c4e50
leandrorafael_24 <leandrorafael_24@hotmail.com>
2017-02-28 23:16:22 -0400

    Cambios

---
+++ Capitulo10/DebugCap12/factorial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Factorial
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            factorial(5);
+            
+        }
+
+        public static void factorial(int n)
+        {
+            int fact = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                fact *= i;
+                Console.WriteLine(fact);
+            }
+            Console.WriteLine("\nEl Factorial de " + n + " es: " + fact);
+             Console.ReadKey();
+            
+        }
+    }
+}
